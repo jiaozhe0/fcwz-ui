d9b51212e30ef85ca2609af36224066e4f61b50b
jiaozhe <946505120@qq.com>
2021-06-22 23:26:33 +0800

    s

--- build/release.sh
+++ build/release.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env sh
 ###
  # @Author: your name
- # @Date: 2021-06-06 13:18:40
- # @LastEditTime: 2021-06-22 22:36:09
- # @LastEditors: Please set LastEditors
+ # @Date: 2021-06-22 23:23:26
+ # @LastEditTime: 2021-06-22 23:26:22
+ # @LastEditors: your name
  # @Description: In User Settings Edit
  # @FilePath: /element/build/release.sh
 ### 
-echo '执行release.sh'
 set -e
-# 我在dev开发
-git checkout main&&
+
+git checkout main
 git merge dev
 
 VERSION=`npx select-version-cli`
-# 设置组件库的版本信息
+
 read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
@@ -25,8 +24,9 @@ then
   VERSION=$VERSION npm run dist
 
   # ssr test
-  # node test/ssr/require.test.js            
-  publish theme 提交样式到
+  node test/ssr/require.test.js            
+
+  # publish theme
   echo "Releasing theme-chalk $VERSION ..."
   cd packages/theme-chalk
   npm version $VERSION --message "[release] $VERSION"
@@ -38,18 +38,16 @@ then
   fi
   cd ../..
 
-  
-  echo "执行git 提交"
   # commit
   git add -A
   git commit -m "[build] $VERSION"
   npm version $VERSION --message "[release] $VERSION"
 
   # publish
-  git push origin main&&
+  git push origin master
   git push origin refs/tags/v$VERSION
   git checkout dev
-  git rebase main&&
+  git rebase master
   git push origin dev
 
   if [[ $VERSION =~ "beta" ]]
